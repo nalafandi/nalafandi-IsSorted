3e61006884ab2812c8f2a963a815ed193263fe7b
nalafandi <niafandi@gmail.com>
2023-02-26 02:06:47 +0000

    Done!

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -6,7 +6,11 @@ public class IsSorted {
      * @param arr an int array
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
-    public boolean checkSort(int[] arr){
-        return false;
-    }
+    public boolean checkSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1])
+                return false;
+        }
+        return true;
+    }   
 }
